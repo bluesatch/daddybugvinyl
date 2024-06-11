5c2542d344ade71808b8145257ce226a0f6da955
H Brown <hebrown12@gmail.com>
2024-06-11 15:03:18 -0500

    hooked up Home.js; minor styling

--- react/src/components/Card.js
+++ react/src/components/Card.js
@@ -6,7 +6,11 @@ const Card =(props)=> {
             <div className="card product-card h-100">
                 <img src={`/images/${props.imgUrl}`} alt={props.productName} className="img-fluid image card-img-top" />
                 <div className="card-body">
-                    <h3 className="card-title product-name text-capitalize">{props.productName}</h3>
+                    <h3 className="card-title product-name text-capitalize">
+                        <Link to={`/${props.productType}/${props.productName}`}>
+                            {props.productName}
+                        </Link>
+                    </h3>
                     <p className="card-text text-capitalize card-detail">{props.detail}</p>
                     <p className="card-text">Price ${props.price.toFixed(2)}</p>
                 </div>
--- react/src/components/Home.js
+++ react/src/components/Home.js
@@ -1,9 +1,39 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Home =()=> {
+
+    const [ featuredAlbum, setFeaturedAlbum ] = useState({})
+
+    const [ today, setToday ] = useState(new Date().getUTCDay())
+
+    // setToday(prevState => prevState.getUTCDay())
+
+    useEffect(()=> {
+        
+        axios.get('http://localhost:3005/api/album').then(res => {
+
+            const count = res.data.length
+
+            setFeaturedAlbum(res.data[Math.floor(Math.random() * count)])
+        })
+    }, [today])
+
+    console.log(featuredAlbum)
+
     return (
         <main className="main" id="main">
             <div className="container">
-                <h2>Home</h2>
+                <section className="section banner-row">
+                    <img src="https://via.placeholder.com/50x50" alt="banner image" className="img-fluid image banner-img rounded" />  
+                </section>
+                <section className="section">
+                    <h2 className="text-center text-capitalize">welcome to our store!</h2>
+                </section>
+                <section className="section featured-section">
+                    <h2 className="text-center text-capitalize text-danger">featured album of the day</h2>
+                    <img src={`/images/${featuredAlbum.imgUrl}`} alt={featuredAlbum.title} className="d-block mx-auto featured-album-img rounded" />
+                </section>
             </div>
         </main>
     )
